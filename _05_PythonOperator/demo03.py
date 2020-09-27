16ed8b726db42cf860542b24d8bed517d772de7f
cyjhunnyboy <cyjhunnyboy.com>
2020-09-27 10:32:31 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

---
+++ _05_PythonOperator/demo03.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+# author: chen_yong_jun
+
+"""
+Python赋值运算符
+=       简单的赋值运算符     c = a + b 将 a + b 的运算结果赋值为 c
++=      加法赋值运算符      c += a 等效于 c = c + a
+-=      减法赋值运算符      c -= a 等效于 c = c - a
+*=      乘法赋值运算符      c *= a 等效于 c = c * a
+/=      除法赋值运算符      c /= a 等效于 c = c / a
+%=      取模赋值运算符      c %= a 等效于 c = c % a
+**=     幂赋值运算符        c **= a 等效于 c = c ** a
+//=     取整除赋值运算符     c //= a 等效于 c = c // a
+"""
+a = 21
+b = 10
+c = 0
+
+d = a + b
+print("1 - d 的值为：", d)
+
+c += a
+print("2 - c 的值为：", c)
+
+c *= a
+print("3 - c 的值为：", c)
+
+c /= a
+print("4 - c 的值为：", c)
+
+c = 2
+c %= a
+print("5 - c 的值为：", c)
+
+c **= a
+print("6 - c 的值为：", c)
+
+c //= a
+print("7 - c 的值为：", c)
