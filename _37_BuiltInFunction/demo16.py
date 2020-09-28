d7ae401244490d98a10646c5237308000b7ab511
cyjhunnyboy <cyjhunnyboy.com>
2020-09-28 16:13:17 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

---
+++ _37_BuiltInFunction/demo16.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# author: chen_yong_jun
+
+'''
+Python id() 函数
+描述：
+    id() 函数返回对象的唯一标识符，标识符是一个整数
+    CPython 中 id() 函数用于获取对象的内存地址
+语法：
+    id([object])
+    参数说明：
+        object -- 对象
+返回值：
+    返回对象的内存地址
+'''
+a = "runoob"
+print(id(a))
+b = 1
+print(id(b))
