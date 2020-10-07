4df7a30db566accc6193def7dd2fd76c21eb7f87
cyjhunnyboy <cyjhunnyboy@sina.com>
2020-10-07 18:11:26 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _37_BuiltInFunction/demo51.py
+++ _37_BuiltInFunction/demo51.py
@@ -22,21 +22,22 @@ class C(object):
     def __init__(self):
         self._x = None
 
-    def getx(self):
+    @property
+    def x(self):
+        """I'm the 'x' property."""
         return self._x
 
-    def setx(self, value):
+    @x.setter
+    def x(self, value):
         self._x = value
 
-    def delx(self):
+    @x.deleter
+    def x(self):
         del self._x
 
-    x = property(getx, setx, delx, "I'm the 'x' property.")
 
-
-# 如果 c 是 C 的实例化, c.x 将触发 getter,c.x = value 将触发 setter ， del c.x 触发 deleter。
-# 如果给定 doc 参数，其将成为这个属性值的 docstring，否则 property 函数就会复制 fget 函数的 docstring（如果有的话）
+# 上面的代码将 voltage() 方法转化成同名只读属性的 getter 方法
 if __name__ == "__main__":
     c = C()
-    c.x = "Python"
+    c.x = "I'm Python"
     print(c.x)
