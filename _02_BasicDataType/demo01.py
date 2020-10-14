3fdfda2dea6612c007d90588899b6785f464635c
cyjhunnyboy <cyjhunnyboy@sina.com>
2020-10-14 23:12:38 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _02_BasicDataType/demo01.py
+++ _02_BasicDataType/demo01.py
@@ -1,46 +1,50 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
 
-"""
-Python3 基本数据类型
-Python 中的变量不需要声明。每个变量在使用前都必须赋值，变量赋值以后该变量才会被创建。
+if __name__ == "__main__":
+    '''
+    Python3 基本数据类型
+    Python 中的变量不需要声明。每个变量在使用前都必须赋值，变量赋值以后该变量才会被创建。
+    在 Python 中，变量就是变量，它没有类型，我们所说的"类型"是变量所指的内存中对象的类型。
+    等号（=）用来给变量赋值。
+    等号（=）运算符左边是一个变量名，等号（=）运算符右边是存储在变量中的值。例如：
+    '''
+    # 整型变量
+    counter = 100
+    # 浮点型变量
+    miles = 1000.0
+    # 字符串
+    name = "runoob"
+    print(counter)
+    print(miles)
+    print(name)
 
-在 Python 中，变量就是变量，它没有类型，我们所说的"类型"是变量所指的内存中对象的类型。
+    '''
+    多个变量赋值
+    Python允许你同时为多个变量赋值。例如：
+    以上实例，创建一个整型对象，值为 1，从后向前赋值，三个变量被赋予相同的数值
+    '''
+    a = b = c = 1
+    print("a = ", a)
+    print("b = ", b)
+    print("c = ", c)
 
-等号（=）用来给变量赋值。
+    # 也可以为多个对象指定多个变量
+    d, e, f = 1, 2, "runoob"
+    print("d = ", d)
+    print("e = ", e)
+    print("f = ", f)
 
-等号（=）运算符左边是一个变量名,等号（=）运算符右边是存储在变量中的值。例如：
-"""
-counter = 100  # 整型变量
-miles = 1000.0  # 浮点型变量
-name = "runoob"  # 字符串
-
-print(counter)
-print(miles)
-print(name)
-
-"""
-多个变量赋值
-Python允许你同时为多个变量赋值。例如：
-以上实例，创建一个整型对象，值为 1，从后向前赋值，三个变量被赋予相同的数值
-"""
-a = b = c = 1
-
-# 也可以为多个对象指定多个变量
-d, e, f = 1, 2, "runoob"
-
-"""
-标准数据类型
-Python3 中有六个标准的数据类型：
-
-Number（数字）
-String（字符串）
-List（列表）
-Tuple（元组）
-Set（集合）
-Dictionary（字典）
-Python3 的六个标准数据类型中：
-
-不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
-可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
-"""
+    '''
+    标准数据类型
+    Python3 中有六个标准的数据类型：
+        Number（数字）
+        String（字符串）
+        List（列表）
+        Tuple（元组）
+        Set（集合）
+        Dictionary（字典）
+    Python3 的六个标准数据类型中：
+        不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+        可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+    '''
--- _02_BasicDataType/demo02.py
+++ _02_BasicDataType/demo02.py
@@ -1,30 +1,6 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
 
-"""
-Number（数字）
-Python3 支持 int、float、bool、complex（复数）。
-
-在Python 3里，只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。
-
-像大多数语言一样，数值类型的赋值和计算都是很直观的。
-
-内置的 type() 函数可以用来查询变量所指的对象类型。
-"""
-a, b, c, d = 20, 5.5, True, 4+3j
-print(type(a), type(b), type(c), type(d))
-a = 111
-print(isinstance(a, int))
-
-# isinstance 和 type 的区别在于：
-"""
-区别就是:
-type()不会认为子类是一种父类类型。
-isinstance()会认为子类是一种父类类型。
-注意：在 Python2 中是没有布尔型的，它用数字 0 表示 False，用 1 表示 True。到 Python3 中，
-把 True 和 False 定义成关键字了，但它们的值还是 1 和 0，它们可以和数字相加。
-"""
-
 
 class A:
     pass
@@ -34,38 +10,60 @@ class B(A):
     pass
 
 
-print(isinstance(A(), A))    # return True
-print(isinstance(B(), A))    # return True
-print(type(A()) == A)        # return True
-print(type(B()) == A)        # return False
+if __name__ == "__main__":
+    '''
+    Number（数字）
+    Python3 支持 int、float、bool、complex（复数）。
+    在Python 3里，只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。
+    像大多数语言一样，数值类型的赋值和计算都是很直观的。
+    内置的 type() 函数可以用来查询变量所指的对象类型。
+    '''
+    a, b, c, d = 20, 5.5, True, 4 + 3j
+    print(type(a), type(b), type(c), type(d))
+    a = 111
+    print(isinstance(a, int))
 
+    # isinstance 和 type 的区别在于：
+    '''
+    区别就是:
+    type()不会认为子类是一种父类类型。
+    isinstance()会认为子类是一种父类类型。
+    注意：在 Python2 中是没有布尔型的，它用数字 0 表示 False，用 1 表示 True。到 Python3 中，
+    把 True 和 False 定义成关键字了，但它们的值还是 1 和 0，它们可以和数字相加。
+    '''
+    print(isinstance(A(), A))  # return True
+    print(isinstance(B(), A))  # return True
+    print(type(A()) == A)  # return True
+    print(type(B()) == A)  # return False
 
-# 当你指定一个值时，Number 对象就会被创建：
-# del语句删除一些对象引用
-var1 = 1
-var2 = 10
-del var1
+    # 当你指定一个值时，Number 对象就会被创建：
+    # del语句删除一些对象引用
+    # 语法：del var1[,var2[,var3[....,varN]]]
+    var1 = 1
+    var2 = 10
+    # 使用del语句删除单个或多个对象
+    del var1, var2
 
-# 数值运算
-a = 5 + 4       # 加法
-b = 4.3 - 2     # 减法
-c = 3 * 7       # 乘法
-d = 2 / 4       # 除法，得到一个浮点数
-e = 2 // 4      # 除法，得到一个整数
-f = 17 % 3      # 取余
-g = 2 ** 5      # 乘方
+    '''
+    注意：
+    1、Python可以同时为多个变量赋值，如a, b = 1, 2。
+    2、一个变量可以通过赋值指向不同类型的对象。
+    3、数值的除法包含两个运算符：/ 返回一个浮点数，// 返回一个整数。
+    4、在混合计算时，Python会把整型转换成为浮点数。
+   '''
+    # 数值运算
+    a = 5 + 4  # 加法
+    b = 4.3 - 2  # 减法
+    c = 3 * 7  # 乘法
+    d = 2 / 4  # 除法，得到一个浮点数
+    e = 2 // 4  # 除法，得到一个整数
+    f = 17 % 3  # 取余
+    g = 2 ** 5  # 乘方
 
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
-print(g)
-"""
-注意：
-1、Python可以同时为多个变量赋值，如a, b = 1, 2。
-2、一个变量可以通过赋值指向不同类型的对象。
-3、数值的除法包含两个运算符：/ 返回一个浮点数，// 返回一个整数。
-4、在混合计算时，Python会把整型转换成为浮点数。
-"""
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
