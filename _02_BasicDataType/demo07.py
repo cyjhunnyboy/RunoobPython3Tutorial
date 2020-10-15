5bcc606aa27eb6708ab0e4ef38de94115a30e4a9
cyjhunnyboyy <cyjhunnyboy@sina.com>
2020-10-15 16:08:03 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _02_BasicDataType/demo07.py
+++ _02_BasicDataType/demo07.py
@@ -1,41 +1,47 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
 
-"""
-_10_PythonDictionary（字典）
-字典（dictionary）是Python中另一个非常有用的内置数据类型。
+if __name__ == "__main__":
+    '''
+    Dictionary（字典）
+    字典（dictionary）是Python中另一个非常有用的内置数据类型。
+    列表是有序的对象集合，字典是无序的对象集合。
+    两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+    字典是一种映射类型，字典用"{}"标识，它是一个无序的键(key):值(value)对集合。
+    键(key)必须使用不可变类型。
+    在同一个字典中，键(key)必须是唯一的。
+    另外，字典类型也有一些内置的函数，例如clear()、keys()、values()等。
+    
+    注意：
+    1、字典是一种映射类型，它的元素是键值对。
+    2、字典的关键字必须为不可变类型，且不能重复。
+    3、创建空字典使用{}。
+    '''
+    # 创建空字典
+    dict_a = dict()
+    # 字典添加dict[key]=value元素
+    dict_a["one"] = "1 - 菜鸟教程"
+    dict_a[2] = "2 - 菜鸟工具"
 
-列表是有序的对象集合，字典是无序的对象集合。两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+    tiny_dict = {"name": "runoob", "code": 1, "site": "www.runoob.com"}
 
-字典是一种映射类型，字典用"{ }"标识，它是一个无序的键(key) : 值(value)对集合。
+    # 输出键为"one"的值
+    print(dict_a["one"])
+    # 输出键为2的值
+    print(dict_a[2])
+    # 输出完整的字典
+    print(tiny_dict)
+    # 输出所有键
+    print(tiny_dict.keys())
+    # 输出所有值
+    print(tiny_dict.values())
 
-键(key)必须使用不可变类型。
+    # 构造函数dict()可以直接从键值对序列中构建字典
+    dict_b = dict([("Runoob", 1), ("Google", 2), ("Taobao", 3)])
+    print(dict_b)
 
-在同一个字典中，键(key)必须是唯一的。
+    dict_c = {x: x ** 2 for x in (2, 4, 6)}
+    print(dict_c)
 
-另外，字典类型也有一些内置的函数，例如clear()、keys()、values()等。
-
-注意：
-
-1、字典是一种映射类型，它的元素是键值对。
-2、字典的关键字必须为不可变类型，且不能重复。
-3、创建空字典使用 { }。
-"""
-dict_a = dict()
-dict_a['one'] = "1 - 菜鸟教程"
-dict_a[2] = "2 - 菜鸟工具"
-
-tiny_dict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
-
-print(dict_a['one'])  # 输出键为 'one' 的值
-print(dict_a[2])  # 输出键为 2 的值
-print(tiny_dict)  # 输出完整的字典
-print(tiny_dict.keys())  # 输出所有键
-print(tiny_dict.values())  # 输出所有值
-
-dict_b = dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)])
-print(dict_b)
-dict_c = {x: x**2 for x in (2, 4, 6)}
-print(dict_c)
-dict_d = dict(Runoob=1, Google=2, Taobao=3)
-print(dict_d)
+    dict_d = dict(Runoob=1, Google=2, Taobao=3)
+    print(dict_d)
--- _02_BasicDataType/demo14.py
+++ _02_BasicDataType/demo14.py
@@ -1,13 +1,18 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
-"""
-Python tuple() 函数
-描述: tuple 函数将列表转换为元组
-语法:tuple( seq )
-参数:seq -- 要转换为元组的序列
-返回值:返回元组
-"""
-list1 = ['Google', 'Taobao', 'Runoob', 'Baidu']
-tuple1 = tuple(list1)
-print(tuple1)
 
+if __name__ == "__main__":
+    '''
+    Python tuple() 函数
+    描述： 
+        tuple 函数将列表转换为元组
+    语法：
+        tuple( seq )
+        参数：
+            seq -- 要转换为元组的序列
+    返回值：
+        返回元组
+    '''
+    list1 = ["Google", "Taobao", "Runoob", "Baidu"]
+    tuple1 = tuple(list1)
+    print(tuple1)
