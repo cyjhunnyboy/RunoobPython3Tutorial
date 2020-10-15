078e7648ae29733f03fcec4c2d7bf3db00034ee9
cyjhunnyboyy <cyjhunnyboy@sina.com>
2020-10-15 17:06:10 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _01_BasicGrammar/demo07.py
+++ _01_BasicGrammar/demo07.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     '''
     数字(Number)类型
-    python中数字有四种类型：整数、布尔型、浮点数和复数。
-    int(整数), 如 1, 只有一种整数类型int，表示为长整型，没有python2中的Long。
+    Python中数字有四种类型：整数、布尔型、浮点数和复数。
+    int(整数), 如1, 只有一种整数类型int，表示为长整型，没有Python2中的Long。
     bool(布尔), 如 True。
     float(浮点数), 如 1.23、3E-2
     complex(复数), 如 1 + 2j、 1.1 + 2.2j
@@ -13,10 +13,10 @@ if __name__ == "__main__":
 
     '''
     字符串(String)
-    python中单引号和双引号使用完全相同。
+    Python中单引号和双引号使用完全相同。
     使用三引号(单三引号或双三引号)可以指定一个多行字符串。
     转义符 '\'
-    反斜杠可以用来转义，使用r可以让反斜杠不发生转义。 如 r"this is a line with \n" 则\n会显示，并不是换行。
+    反斜杠可以用来转义，使用r可以让反斜杠不发生转义。 如 r"this is a line with\n" 则\n会显示，并不是换行。
     按字面意义级联字符串，如"this " "is " "string"会被自动转换为this is string。
     字符串可以用+运算符连接在一起，用*运算符重复。
     Python中的字符串有两种索引方式，从左往右以0开始，从右往左以-1开始。
@@ -28,8 +28,7 @@ if __name__ == "__main__":
 
     word = "字符串"
     sentence = "这是一个句子。"
-    paragraph = """这是一个段落，
-    可以由多行组成"""
+    paragraph = """这是一个段落，可以由多行组成"""
     print(word)
     print(sentence)
     print(paragraph)
@@ -54,5 +53,5 @@ if __name__ == "__main__":
 
     # 使用反斜杠(\)+n转义特殊字符
     print("hello\nrunoob")
-    # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+    # 在字符串前面添加一个r，表示原始字符串，不会发生转义
     print(r"hello\nrunoob")
