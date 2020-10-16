595128f73183f94093b893f572d6ca3480aef002
cyjhunnyboyy <cyjhunnyboy@sina.com>
2020-10-16 16:52:59 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _07_PythonString/demo02.py
+++ _07_PythonString/demo02.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
-"""
-Python 访问字符串中的值
-Python 不支持单字符类型，单字符在 Python 中也是作为一个字符串使用。
 
-Python 访问子字符串，可以使用方括号来截取字符串
-"""
-var1 = 'Hello World!'
-var2 = "Runoob"
-print("var1[0]: ", var1[0])
-print("var2[1:5]: ", var2[1:5])
+'''
+Python 访问字符串中的值
+    Python不支持单字符类型，单字符在Python中也是作为一个字符串使用。
+    Python访问子字符串，可以使用方括号来截取字符串
+'''
+if __name__ == "__main__":
+    var1 = "Hello World!"
+    var2 = "Runoob"
+    print("var1[0]: ", var1[0])
+    print("var2[1:5]: ", var2[1:5])
--- _07_PythonString/demo03.py
+++ _07_PythonString/demo03.py
@@ -1,9 +1,28 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
 
-"""
-Python字符串更新
-你可以截取字符串的一部分并与其他字段拼接
-"""
-var1 = 'Hello World!'
-print("已更新字符串 : ", var1[:6] + 'Runoob!')
+'''
+Python 字符串更新
+    你可以截取字符串的一部分并与其他字段拼接
+
+Python 转义字符
+    转义字符        描述
+    \(在行尾时)     续行符
+    \\             反斜杠符号
+    \'             单引号
+    \"             双引号
+    \a             响铃
+    \b             退格(Backspace)
+    \000           空
+    \n             换行
+    \v             纵向制表符
+    \t             横向制表符
+    \r             回车
+    \f             换页
+    \oyy           八进制数，yy代表的字符，例如：\o12代表换行，其中o是字母，不是数字0
+    \xyy           十六进制数，yy代表的字符，例如：\x0a代表换行
+    \other         其它的字符以普通格式输出
+'''
+if __name__ == "__main__":
+    var1 = "Hello World!"
+    print("已更新字符串：", var1[:6] + "Runoob!")
