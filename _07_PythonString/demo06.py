26c8b3cf4c396078f8a618ba00e77b7533167154
cyjhunnyboyy <cyjhunnyboy@sina.com>
2020-10-16 18:32:00 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _07_PythonString/demo06.py
+++ _07_PythonString/demo06.py
@@ -1,20 +1,24 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
-"""
-Python三引号
-python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符
-"""
+
+'''
+Python 三引号
+    python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符
+
+    三引号让程序员从引号和特殊字符串的泥潭里面解脱出来，自始至终保持一小块字符串的格式是所谓的WYSIWYG（所见即所得）格式的
+    一个典型的用例是，当你需要一块HTML或者SQL时，这时用字符串组合，特殊字符串转义将会非常的繁琐
+
+    cursor.execute(\'''
+    CREATE TABLE users (
+    login VARCHAR(8),
+    uid INTEGER,
+    prid INTEGER)
+    \''')
+'''
 para_str = """这是一个多行字符串的实例
 多行字符串可以使用制表符
 TAB ( \t )。
 也可以使用换行符 [ \n ]。
-"""
-print(para_str)
-
-"""
-三引号让程序员从引号和特殊字符串的泥潭里面解脱出来，自始至终保持一小块字符串的格式是所谓的WYSIWYG（所见即所得）格式的。
-
-一个典型的用例是，当你需要一块HTML或者SQL时，这时用字符串组合，特殊字符串转义将会非常的繁琐
 """
 
 errHTML = '''
@@ -26,3 +30,7 @@ Friends CGI Demo</TITLE></HEAD>
 ONCLICK="window.history.back()"></FORM>
 </BODY></HTML>
 '''
+
+if __name__ == "__main__":
+    print(para_str)
+    print(errHTML)
--- _07_PythonString/demo08.py
+++ _07_PythonString/demo08.py
@@ -1,11 +1,17 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
 
-"""
-capitalize()
-将字符串的第一个字母变成大写,其他字母变小写
-语法：str.capitalize()
-返回值：该方法返回一个首字母大写的字符串
-"""
-str_a = "this is string example from runoob....wow!!!"
-print("str.capitalize() : ", str_a.capitalize())
+'''
+Python3 capitalize()方法
+描述：
+    Python capitalize()将字符串的第一个字母变成大写,其他字母变小写
+语法：
+    str.capitalize()
+    参数：
+        无
+返回值：
+    该方法返回一个首字母大写的字符串
+'''
+if __name__ == "__main__":
+    str_a = "this is string example from runoob....wow!!!"
+    print("str.capitalize()：", str_a.capitalize())
