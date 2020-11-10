c364e051a8ac839f803ba29068b54d894872fca0
cyjhunnyboyy <cyjhunnyboy@sina.com>
2020-11-10 18:33:09 +0800

    https://www.runoob.com/python3/python3-tutorial.html Python 3 教程

--- _27_RegularExpression/demo07.py
+++ _27_RegularExpression/demo07.py
@@ -1,25 +1,30 @@
 # -*- coding: UTF-8 -*-
 # author: chenyongjun
-"""
+
+'''
 检索和替换
-Python 的re模块提供了re.sub用于替换字符串中的匹配项
-语法：re.sub(pattern, repl, string, count=0)
-参数：
-pattern : 正则中的模式字符串。
-repl : 替换的字符串，也可为一个函数。
-string : 要被查找替换的原始字符串。
-count : 模式匹配后替换的最大次数，默认 0 表示替换所有的匹配。
-"""
-# repl 参数是一个函数
-# 以下实例中将字符串中的匹配的数字乘于 2
+    Python的re模块提供了re.sub用于替换字符串中的匹配项
+语法：
+    re.sub(pattern, repl, string, count=0, flags=0)
+    参数：
+        pattern：正则中的模式字符串。
+        repl：替换的字符串，也可为一个函数。
+        string：要被查找替换的原始字符串。
+        count：模式匹配后替换的最大次数，默认0表示替换所有的匹配。
+        flags：编译时用的匹配模式，数字形式。
+
+        repl参数是一个函数
+'''
 import re
 
 
-# 将匹配的数字乘于 2
 def double(matched):
-    value = int(matched.group('value'))
+    """将匹配的数字乘于2"""
+
+    value = int(matched.group("value"))
     return str(value * 2)
 
 
-s = 'A23G4HFD567'
-print(re.sub('(?P<value>\d+)', double, s))
+if __name__ == "__main__":
+    s = "A23G4HFD567"
+    print(re.sub("(?P<value>\d+)", double, s))
