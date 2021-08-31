1620fdcca5666270387f8a292ee7c7e6d682236d
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 18:00:17 +0800

    Python 3 基础语法（import与from...import）

---
+++ _04_Python3BasicGrammar/demo17.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python3 基础语法
+        1、import与from...import
+            在python用import或者from...import来导入相应的模块。
+
+            将整个模块(somemodule)导入，格式为：import somemodule
+            从某个模块中导入某个函数,格式为：from somemodule import somefunction
+            从某个模块中导入多个函数,格式为：from somemodule import firstfunc, secondfunc, thirdfunc
+            将某个模块中的全部函数导入，格式为：from somemodule import *
+'''
+# 导入sys模块的argv,path成员
+# 导入特定的成员
+from sys import argv, path
+
+if __name__ == "__main__":
+    print("================python from import===================================")
+    # 因为已经导入path成员，所以此处引用时不需要加sys.path
+    print("path:", path)
