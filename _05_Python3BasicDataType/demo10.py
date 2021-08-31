bec3043c41cfef4d4e06880085ee6d03f04723e3
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 16:21:01 +0800

    Python 3 基本数据类型（String字符串）

---
+++ _05_Python3BasicDataType/demo10.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python3 基本数据类型
+        1、String（字符串）
+            加号+是字符串的连接符， 星号*表示复制当前字符串，紧跟的数字为复制的次数。
+'''
+if __name__ == "__main__":
+    str_a = 'Runoob'
+
+    # 输出字符串两次
+    print(str_a * 2)
+
+    # 连接字符串
+    print(str_a + "TEST")
