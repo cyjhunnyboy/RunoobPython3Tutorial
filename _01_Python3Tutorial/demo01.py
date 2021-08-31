d8c3df5b6ced819ab1327e3e8ceb16cba02d1ce1
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:00:55 +0800

    Python 3 教程（第一个Python3.x程序）

--- _01_Python3Tutorial/demo01.py
+++ _01_Python3Tutorial/demo01.py
@@ -7,8 +7,10 @@
         为了不带入过多的累赘，Python 3.0在设计的时候没有考虑向下兼容。
         1、查看Python版本
             命令窗口(Windows使用win+R调出cmd运行框)查看Python版本
-            python -V
+            python -V 或者 python --version
 '''
 if __name__ == "__main__":
     # 第一个Python3.x程序。
+    # 对于大多数程序语言，第一个入门编程代码便是"Hello World!"
+    # 以下代码为使用Python输出"Hello World!"
     print("Hello, World!")
