5180de5a51d9c765773ecc7d528ffcc2b9df0a33
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:03:07 +0800

    Python 3 基本数据类型（数据类型转换：tuple()函数）

---
+++ _05_Python3BasicDataType/demo29.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python数据类型转换
+        Python tuple()函数
+            描述：
+                tuple函数将列表转换为元组
+            语法：
+                tuple(seq)
+                参数：
+                    seq -- 要转换为元组的序列
+            返回值：
+                返回元组
+'''
+if __name__ == "__main__":
+    list1 = ["Google", "Taobao", "Runoob", "Baidu"]
+    tuple1 = tuple(list1)
+    print(tuple1)
