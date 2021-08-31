a4aa7cd656108866b128801a10dbb718e215d374
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:03:28 +0800

    Python 3 基本数据类型（数据类型转换：set()函数）

---
+++ _05_Python3BasicDataType/demo31.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python数据类型转换
+        Python set()函数
+            描述：
+                set()函数创建一个无序不重复元素集，可进行关系测试，删除重复数据，还可以计算交集、差集、并集等
+            语法：
+                class set([iterable])
+                参数：
+                    iterable -- 可迭代对象对象
+            返回值：
+                返回新的集合对象
+'''
+if __name__ == "__main__":
+    x = set("runoob")
+    y = set("google")
+    # 重复的被删除
+    print(x, y)
+    # 交集
+    print(x & y)
+    # 并集
+    print(x | y)
+    # 差集
+    print(x - y)
