6e63cca1e0d301f19aeafe5636bda664fab887b1
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:03:56 +0800

    Python 3 基本数据类型（数据类型转换：frozenset()函数）

---
+++ _05_Python3BasicDataType/demo33.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python数据类型转换
+        Python frozenset()函数
+            描述：
+                frozenset()返回一个冻结的集合，冻结后集合不能再添加或删除任何元素
+            语法：
+                class frozenset([iterable])
+                参数：
+                    iterable -- 可迭代的对象，比如列表、字典、元组等等
+            返回值：
+                返回新的 frozenset 对象，如果不提供任何参数，默认会生成空集合
+'''
+if __name__ == "__main__":
+    # 生成一个新的不可变集合
+    a = frozenset(range(10))
+    print(a)
+    # 创建不可变集合
+    b = frozenset('runoob')
+    print(b)
