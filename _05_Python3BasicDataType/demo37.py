4862612bcee7cb86df896433d221cbe364f51a7c
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:04:39 +0800

    Python 3 基本数据类型（数据类型转换：oct()函数）

---
+++ _05_Python3BasicDataType/demo37.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python数据类型转换
+        Python oct()函数
+            描述：
+                oct()函数将一个整数转换成8进制字符串
+            语法：
+                oct(x)
+                参数：
+                    x -- 整数
+            返回值：
+                返回8进制字符串
+'''
+if __name__ == "__main__":
+    print(oct(10))
+    print(oct(20))
+    print(oct(15))
