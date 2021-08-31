c5fd4f268d1b329b4209514820db7328681edf06
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:58:42 +0800

    Python 3 基础语法（String字符串）

--- _04_Python3BasicGrammar/demo11.py
+++ _04_Python3BasicGrammar/demo11.py
@@ -3,27 +3,12 @@
 
 '''
     Python3 基础语法
-        1、import与from...import
-            在python用import或者from...import来导入相应的模块。
-
-            将整个模块(somemodule)导入，格式为：import somemodule
-            从某个模块中导入某个函数,格式为：from somemodule import somefunction
-            从某个模块中导入多个函数,格式为：from somemodule import firstfunc, secondfunc, thirdfunc
-            将某个模块中的全部函数导入，格式为：from somemodule import *
+        2、字符串(String)
+            使用反斜杠(\)+n转义特殊字符
+            在字符串前面添加一个r，表示原始字符串，不会发生转义
 '''
-# 导入sys模块
-import sys
-# 导入sys模块的argv,path成员
-# 导入特定的成员
-from sys import argv, path
-
 if __name__ == "__main__":
-    print("================Python import mode==========================")
-    print("命令行参数为:")
-    for i in sys.argv:
-        print(i)
-
-    print("\n python 路径为", sys.path)
-    print("================python from import===================================")
-    # 因为已经导入path成员，所以此处引用时不需要加sys.path
-    print("path:", path)
+    # 使用反斜杠(\)+n转义特殊字符
+    print("hello\nrunoob")
+    # 在字符串前面添加一个r，表示原始字符串，不会发生转义
+    print(r"hello\nrunoob")
