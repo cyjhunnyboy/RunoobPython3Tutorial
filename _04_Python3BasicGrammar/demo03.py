0fcf647bd4dec60ff3e684a5f53ced5b8756d97b
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 16:13:18 +0800

    Python 3 基础语法（注释）

---
+++ _04_Python3BasicGrammar/demo03.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python3 基础语法
+        1、注释
+            1）Python中单行注释以#开头
+            2）多行注释可以用多个#号，还有\'''和\""""
+'''
+if __name__ == "__main__":
+    # 第一个注释
+    print("Hello, Welcome to Python!")  # 第二个注释
