9b043a7795b9ce71ad9508d5ce40e90c7be61dc3
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 16:08:33 +0800

    Python 3 教程

---
+++ _01_Python3Tutorial/demo01.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python 3教程
+        Python的3.0版本，常被称为Python 3000，或简称Py3k。相对于Python的早期版本，这是一个较大的升级。
+        为了不带入过多的累赘，Python 3.0在设计的时候没有考虑向下兼容。
+        1、查看Python版本
+            命令窗口(Windows使用win+R调出cmd运行框)查看Python版本
+            python -V
+'''
+if __name__ == "__main__":
+    # 第一个Python3.x程序。
+    print("Hello, World!")
