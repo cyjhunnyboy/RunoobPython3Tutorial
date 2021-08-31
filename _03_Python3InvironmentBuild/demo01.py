7f7a5456d19be42602cce081058f7391c401c314
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 16:10:27 +0800

    Python 3 环境搭建

---
+++ _03_Python3InvironmentBuild/demo01.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+# author: chenyongjun
+
+'''
+    Python3 环境搭建
+        1、Python3 下载
+            Python官网：https://www.python.org/
+            Python文档下载地址：https://www.python.org/doc/
+        2、Python 安装
+        3、环境变量配置
+            1）系统变量：PYTHON_HOME=D:\Develop\Python\Python38
+            2）Path变量：%PYTHON_HOME%;%PYTHON_HOME%\Scripts;
+            3）查看Python版本：python -V
+'''
+if __name__ == "__main__":
+    print("Hello, World!")
