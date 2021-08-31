f69b5c2d37d7ebf12e7ed198479a78502b191f49
cyjhunnyboy <cyjhunnyboy@sina.com>
2021-08-31 17:52:18 +0800

    Python 3 基础语法（String字符串）

--- _04_Python3BasicGrammar/demo09.py
+++ _04_Python3BasicGrammar/demo09.py
@@ -3,22 +3,16 @@
 
 '''
     Python3 基础语法
-        1、多个语句构成代码组
-            缩进相同的一组语句构成一个代码块，我们称之代码组。
-            像if、while、def和class这样的复合语句，首行以关键字开始，以冒号(:)结束，该行之后的一行或多行代码构成代码组。
-            我们将首行及后面的代码组称为一个子句(clause)。
-
-            如下实例：
-                if expression :
-                   suite
-                elif expression :
-                   suite
-                else :
-                   suite
+        2、字符串(String)
+            转义符 '\'
+            反斜杠可以用来转义，使用r可以让反斜杠不发生转义。 如 r"this is a line with\n" 则\n会显示，并不是换行。
+            按字面意义级联字符串，如"this " "is " "string"会被自动转换为this is string。
 '''
 if __name__ == "__main__":
-    num = 12
-    if num % 2 == 0:
-        print("num = %d 是偶数" % num)
-    else:
-        print("num = %d是奇数" % num)
+    # 转义符"\"，反斜杠可以用来转义，"\n"表示换行
+    str_1 = "this is a line with \n"
+    print(str_1)
+
+    # 使用r可以让反斜杠不发生转义，"\n"并不换行，直接输出"\n"
+    str_2 = r"this is a line with \n"
+    print(str_2)
