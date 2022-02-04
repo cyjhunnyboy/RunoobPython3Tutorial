8c7d7a873fda5dcec57c031d800cfdb539c43f93
cyjhunnyboy <cyjhunnyboy@sina.com>
2022-02-04 16:54:32 +0800

    Python 3 教程

--- _04_Python3BasicGrammar/demo13.py
+++ _04_Python3BasicGrammar/demo13.py
@@ -9,4 +9,4 @@
 if __name__ == "__main__":
     # 同一行显示多条语句
     # Python可以在同一行中使用多条语句，语句之间使用分号(;)分割，以下是一个简单的实例：
-    import sys;x="runoob";sys.stdout.write(x+"\n")
+    import sys; x="runoob"; sys.stdout.write(x+"\n")
